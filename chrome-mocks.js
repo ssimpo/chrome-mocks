e8a40a850d298304211df250b3bd6d04b02d3319
stephen <me@simpo.org>
2014-03-09 15:50:21 +0000

    Updated the mocks to give better handling of message post mocking.

--- chrome-mocks.js
+++ chrome-mocks.js
@@ -1,6 +1,23 @@
+"use strict";
+
 var chrome = {};
 
 chrome.runtime = {
 	sendMessage: function(){
+	},
+	onMessage: {
+		addListener: function(callback){
+			chromeMock.onMessage.push(callback);
+		}
 	}
 }
+
+var chromeMock = {
+	onMessage: [],
+	onMessageEvent: function(data) {
+		chromeMock.onMessage.forEach(function(callback){
+			callback(data);
+		});
+	}
+}
+
